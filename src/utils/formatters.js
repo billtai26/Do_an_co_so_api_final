3a2270062a9d7beb67a4be10c30f8e31729f84dd
billtai26 <anhtaipxuan@gmail.com>
2025-05-16 23:43:33 +0700

    Lesson 05: Create APIs Register

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,6 +2,8 @@
  * YouTube: TrungQuanDev - Một Lập Trình Viên
  * Created by trungquandev.com's author on Sep 27, 2023
  */
+import { pick } from 'lodash'
+
 /**
  * Simple method to Convert a String to Slug
  * Các bạn có thể tham khảo thêm kiến thức liên quan ở đây: https://byby.dev/js-slugify-string
@@ -17,4 +19,10 @@ export const slugify = (val) => {
     .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
     .replace(/\s+/g, '-') // replace spaces with hyphens
     .replace(/-+/g, '-') // remove consecutive hyphens
+}
+
+// Lấy một vài dữ liệu cụ thể trong User để tránh việc trả về các dữ liệu nhạy cảm như hash password
+export const pickUser = (user) => {
+  if (!user) return {}
+  return pick(user, ['_id', 'email', 'username', 'displayname', 'avartar', 'role', 'isActive', 'createdAt', 'updatedAt'])
 }
